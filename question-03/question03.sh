57c1d3edee97b44877b57ffabab255dbc171ec33
ciaccia95 <antonellociaccia9@gmail.com>
2023-12-20 10:09:35 +0100

    I modifyu question03 script

--- question-03/question03.sh
+++ question-03/question03.sh
@@ -3,11 +3,5 @@
 # Nome del nodo da verificare
 NODE_NAME="worker01"
 
-# Ottieni lo stato del nodo
-NODE_STATUS=$(kubectl get nodes "$NODE_NAME" -o=jsonpath='{.status.conditions[?(@.type=="Ready")].status}')
-
-# Verifica lo stato del nodo
-if [ "$NODE_STATUS" != "True" ]; then
-    # Esegui l'uncordon sul nodo
-    kubectl uncordon "$NODE_NAME" > /dev/null
-fi
+# Esegui l'uncordon sul nodo
+kubectl uncordon "$NODE_NAME" > /dev/null

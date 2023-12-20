5f3a1fea0c84b594fb2333e3db65b1f8d6b3de38
ciaccia95 <antonellociaccia9@gmail.com>
2023-12-20 10:48:23 +0100

    Fixing question08

--- question-08/question08.sh
+++ question-08/question08.sh
@@ -5,7 +5,7 @@ NODE_NAME="worker01"
 
 # Label da aggiungere
 LABEL_KEY="scope"
-LABEL_VALUE="prd"
+LABEL_VALUE="prod"
 
 # Aggiungi la label al nodo
 kubectl label nodes "$NODE_NAME" "$LABEL_KEY"="$LABEL_VALUE" --overwrite &> /dev/null
